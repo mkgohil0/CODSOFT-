37579cf8cb20a4a8c3c49998cebb994eafb2997c
mkgohil0 <gohil7256@gmail.com>
2025-07-26 14:59:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,7 @@
  * This file handles the interactive elements of the Shuddh Organic website.
  */
 
-document.addEventListener('DOMContentLoaded', function() {
-    const firebaseConfig = {
-  apiKey: "AIzaSy...YOUR_KEY_HERE...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+
 };
 
     // --- Hero Banner Background Color Changer ---
