7d086645bbdd2c2f31729848316a79074cb24849
mkgohil0 <gohil7256@gmail.com>
2025-07-26 15:51:45 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
 // IMPORTANT: PASTE YOUR FIREBASE CONFIGURATION KEYS HERE
-const firebaseConfig = {
-    apiKey: "PASTE_YOUR_API_KEY_HERE",
-    authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-    projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-    storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-    messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-    appId: "PASTE_YOUR_APP_ID_HERE"
+ const firebaseConfig = {
+  apiKey: "AIzaSyBDPOqS36Ka9hNCfTuyCbhdgpn2dmIIw-o",
+  authDomain: "shuddh-organic-store.firebaseapp.com",
+  projectId: "shuddh-organic-store",
+  storageBucket: "shuddh-organic-store.firebasestorage.app",
+  messagingSenderId: "48476584230",
+  appId: "1:48476584230:web:a5ecb53d8adc3e3b6c69e7",
+  measurementId: "G-5WW4GGL034"
 };
-
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
